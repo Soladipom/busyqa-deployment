8bed6d167b58b7c8bd46445b3630ffbaaeffa766
SAMUEL MAYOWA OLADIPO <mayonator7@yahoo.com>
2024-10-02 22:49:34 -0400

    Update node_group.tf

--- eks-cluster/terraform/node_group.tf
+++ eks-cluster/terraform/node_group.tf
@@ -5,40 +5,13 @@ resource "aws_eks_node_group" "example" {
   subnet_ids      = [local.subnet_a, local.subnet_d, local.subnet_b]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
   }
 
   update_config {
-    max_unavailable = 1
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
-
-
-ubuntu@k8s:~/Terraform/EKS$ cat node_group.tf 
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "main-node-group"
-  node_role_arn   = "aws_iam_role.node_example.arn"
-  subnet_ids      = [local.subnet_a, local.subnet_d, local.subnet_b]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
+    max_unavailable = 2
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
