cffe0905c8789ee0cf269723ef9aedb537c28e85
SAMUEL MAYOWA OLADIPO <mayonator7@yahoo.com>
2024-10-02 22:17:04 -0400

    Update node-group.tf

--- eks-cluster/terraform/node-group.tf
+++ eks-cluster/terraform/node-group.tf
@@ -1,7 +1,6 @@
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_role       = var.node_group_role
-  node_role    = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = aws_subnet.private_subnet[*].id
   scaling_config {
     desired_size = var.desired_capacity
