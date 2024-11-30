1ea999b1a8d3a9ef28b9da62abcb85023c81d510
SAMUEL MAYOWA OLADIPO <mayonator7@yahoo.com>
2024-11-30 08:34:28 -0500

    Create locals.tf

---
+++ eks-cluster/modular/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env         = "test"
+  region      = "ca-central-1"
+  zone1       = "ca-central-1a"
+  zone2       = "ca-central-1b"
+  eks_name    = "main-demo"
+  eks_version = "1.29"
+}
